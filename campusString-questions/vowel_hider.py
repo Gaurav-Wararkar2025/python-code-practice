a1fda45ac9ee1b34f274e34674d53f5c493f0852
Gaurav Wararkar <gauravwararkar143@gmail.com>
2026-01-18 00:31:17 +0530

    The Vowel Hider

---
+++ campusString-questions/vowel_hider.py
@@ -0,0 +1,12 @@
+def hide_vowels(s):
+    vowels = "aeiouAEIOU"
+    result = ""
+
+    for ch in s:
+        if ch in vowels:
+            result += "*"
+        else:
+            result += ch
+
+    return result
+print(hide_vowels("Hello World!"))  # Example usage
