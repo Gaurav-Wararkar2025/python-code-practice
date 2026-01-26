885a895101b85aae019200ec55d737ff8005ff51
Gaurav Wararkar <gauravwararkar143@gmail.com>
2026-01-26 22:58:55 +0530

    Move zeros to the end in array

--- CampusArray-questions/move_zero-to-end.py
+++ CampusArray-questions/move_zero-to-end.py
@@ -6,9 +6,24 @@ def move_zeros_to_end(arr):
             arr[i], arr[j] = arr[j], arr[i]
             j += 1
 
-
 n = int(input("Enter the number of elements: "))
 arr = list(map(int, input("Enter the elements: ").split()))
 
 move_zeros_to_end(arr)
 print(*arr)
+
+"""
+arr = [0, 1, 0, 3, 12]
+
+index = 0
+for i in arr:
+    if i != 0:
+        arr[index] = i
+        index += 1
+
+for i in range(index, len(arr)):
+    arr[i] = 0
+
+print(arr)
+
+"""
