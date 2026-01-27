01244197ba7b91c8bf4ade8557fd11aa22ecafb8
Gaurav Wararkar <gauravwararkar143@gmail.com>
2026-01-27 22:05:57 +0530

    To reverse an array

--- CampusArray-questions/reverse_array.py
+++ CampusArray-questions/reverse_array.py
@@ -1,6 +1,6 @@
 #Reverse an Array In-Place
 
-arr = [1, 2, 3, 4, 5]
+arr = [6, 2, 3, 4, 5]
 
 left, right = 0, len(arr) - 1
 while left < right:
@@ -12,7 +12,7 @@ print(arr)
 
 """
 
-Method 1: Using a Temporary Array
+Method 2: Using a Temporary Array
 arr = [1, 2, 3, 4]
 arr.reverse()
 print(arr)
@@ -20,5 +20,19 @@ print(arr)
 Method 3: Using Slicing
 arr = [1, 2, 3, 4]
 print(arr[::-1])
+
+Method 4: Using another array
+arr=[4,5,6,7,8,9]
+arr2=(arr[-1:0:-1])
+print(arr2)
+
+
+#Method 5: Using Recursion
+
+def reverse_array_recursive(arr, left, right):
+    if left >= right:
+        return
+    arr[left], arr[right] = arr[right], arr[left]
+    reverse_array_recursive(arr, left + 1, right - 1)
 
 """
