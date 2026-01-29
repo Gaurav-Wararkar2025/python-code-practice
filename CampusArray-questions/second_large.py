61311397d8805810abb6fd5af54ec7ddf696d42a
Gaurav Wararkar <gauravwararkar143@gmail.com>
2026-01-29 23:18:55 +0530

    To find second largest element in the array

--- CampusArray-questions/second_large.py
+++ CampusArray-questions/second_large.py
@@ -1,5 +1,33 @@
-arr = [10, 20, 4, 45, 99]
+arr = [10, 20, 75,4, 45, 99]
 arr = list(set(arr))
 arr.sort()
 print("Array is:",arr)
 print("Second largest element is: ",arr[-2])
+
+
+"""
+#Method 2 without sorting
+
+def second_largest(arr):
+    first = second = 0
+    
+    for num in arr:
+        if num > first:
+            second = first
+            first = num
+        elif first > num > second:
+            second = num
+            
+    return second
+
+arr = [10, 20, 4,75, 45, 99]
+print("Second largest element is:", second_largest(arr))
+
+
+#Method 3 using max() function
+
+arr = [10, 20, 4,75, 45, 99]
+arr.remove(max(arr))
+print("Second largest element is:", max(arr))
+
+"""
