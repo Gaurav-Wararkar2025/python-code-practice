0cd3bb3d3dbc9c415fb5c27adf5516a9ae4d0000
Gaurav Wararkar <gauravwararkar143@gmail.com>
2026-01-31 22:07:00 +0530

    To count vowels in a string

--- campusString-questions/count_vowels.py
+++ campusString-questions/count_vowels.py
@@ -4,3 +4,20 @@ for ch in s:
     if ch in "aeiou":
         count += 1
 print(count)
+
+"""
+s = "programming"
+vowels = "aeiouAEIOU"
+v = c = 0
+
+for ch in s:
+    if ch.isalpha():
+        if ch in vowels:
+            v += 1
+        else:
+            c += 1
+
+print("Vowels:", v)
+print("Consonants:", c)
+
+"""
